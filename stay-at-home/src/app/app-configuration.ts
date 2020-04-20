c1e50253f9fc7556545543e735e4d52772b17a8f
Luis Antonio Peregrina <luis_andrade@persistent.co.in>
2020-04-20 10:25:53 -0500

    Add jsdoc to app config

--- stay-at-home/src/app/app-configuration.ts
+++ stay-at-home/src/app/app-configuration.ts
@@ -1,9 +1,39 @@
 export class AppConfiguration {
-    public static GPS_CHECK_POSITION_TIMEOUT: number = 5000;
-    public static GPS_CHECK_POSITION_BACKGROUND_TIMEOUT: number = 300000;
-    public static DISTANCE_THRESHOLD: number = 10;
-    public static DISTANCE_TO_HOUSE_THRESHOLD: number = 30;
+    // Some details on the app
+    /**
+     * The IndexedDB will be saved like so under the device.
+     */
     public static APP_DB_SCHEMA: string = 'c4c_mx_covid19'
 
+    // Some business rules
+    /**
+     * Foreground timeout to trigger the GPS service.
+     */
+    public static GPS_CHECK_POSITION_TIMEOUT: number = 5000;
+    /**
+     * Background timeout to trigger the GPS service.
+     */
+    public static GPS_CHECK_POSITION_BACKGROUND_TIMEOUT: number = 300000;
+
+    /**
+     * Distance, in meters, to trigger the status from Growing to Shrinking.
+     */
+    public static DISTANCE_TO_HOUSE_THRESHOLD: number = 30;
+
+    /**
+     * Time, in milliseconds, that takes to get a new tree.
+     */
     public static TIME_TO_GROW_TREE: number = 10000;
+
+    /**
+     * Working hours, in ms, time being from 0:00 to 23:59.
+     */
+    static get WORKING_HOURS() {
+        let today = new Date();
+        today.setHours(0, 0, 0);
+        let start = today;
+        today.setHours(23, 59, 59);
+        let end = today;
+        return { start: start, end: end };
+    }
 }
