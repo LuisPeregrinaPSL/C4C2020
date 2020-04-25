4a42722df4bd5512fc2abe3084105f56a4014bc4
Luis Antonio Peregrina <luis_andrade@persistent.co.in>
2020-04-25 14:26:55 -0500

    Use VR in window

--- stay-at-home/src/app/app.component.ts
+++ stay-at-home/src/app/app.component.ts
@@ -3,10 +3,8 @@ import { Platform } from '@ionic/angular';
 import { Plugins } from '@capacitor/core';
 import { TabsService } from './services/tabs.service';
 const { SplashScreen } = Plugins;
-import { BackgroundGeolocation, BackgroundGeolocationEvents, BackgroundGeolocationResponse, BackgroundGeolocationLocationProvider } from '@ionic-native/background-geolocation/ngx';
-import { AppConfiguration } from './app-configuration';
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation/ngx';
 import { GpsService } from './services/gps.service';
-import { SimpleCoordinates } from './simple-coordinates';
 
 @Component({
   selector: 'app-root',
@@ -24,25 +22,8 @@ export class AppComponent {
   }
 
   initializeApp() {
-    this.backgroundGeolocation.configure({
-      locationProvider: BackgroundGeolocationLocationProvider.RAW_PROVIDER,
-      desiredAccuracy: 10,
-      distanceFilter: 0,
-      interval: AppConfiguration.GPS_CHECK_POSITION_BACKGROUND_TIMEOUT,
-      fastestInterval: AppConfiguration.GPS_CHECK_POSITION_TIMEOUT,
-      notificationsEnabled: true,
-      notificationTitle: 'Background tracking',
-      notificationText: 'enabled',
-      debug: true
-    });
     this.platform.ready().then(() => {
       SplashScreen.hide();
-      this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
-        this.backgroundGeolocation.startTask().then((taskKey) => {
-          this.gpsSvc.mainLoop(new SimpleCoordinates(location.latitude, location.longitude));
-          this.backgroundGeolocation.endTask(taskKey);
-        });
-      });
     });
   }
 }
