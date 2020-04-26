32dfdd21782b6fd5627e9d180dab6ab14fbda2ad
Luis Antonio Peregrina <luis_andrade@persistent.co.in>
2020-04-25 19:10:54 -0500

    Prod build 2020_04_25-1510

---
+++ stay-at-home-app/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const app = express();
+const path = require('path')
+
+// Ionic CORS
+app.use(function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET,POST');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+var port = process.env.PORT || 3000
+http.createServer(app).listen(port);
+//https.createServer(options, app).listen(443);
+console.log('Port is ' + port);
