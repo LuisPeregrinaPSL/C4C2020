d4bb1e880089a0557ce3d34ed0a66016b28e7c69
Jaime de Alba <jaime_alba@persistent.com>
2020-04-27 14:54:19 -0500

    adding a missing tree

--- stay-at-home/src/app/forest.types.ts
+++ stay-at-home/src/app/forest.types.ts
@@ -21,7 +21,9 @@ models['bubinga'] = {name: 'Bubinga', label: 'Bubinga', gltfModel: 'bubinga', id
 models['willow'] = {name: 'Willow', label: 'Willow', gltfModel: 'willow', id: 'treeWillow', src: '/assets/gltf/willow.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'Willows, also called sallows and osiers, form the genus Salix, around 400 species of deciduous trees and shrubs, found primarily on moist soils in cold and temperate regions of the Northern Hemisphere.'};
 models['apple'] = {name: 'Apple', label: 'Apple', gltfModel: 'apple', id: 'treeApple', src: '/assets/gltf/apple.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'An apple is a sweet, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.'};
 models['common'] = {name: 'Common', label: 'Common', gltfModel: 'common', id: 'treeCommon', src: '/assets/gltf/common.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'I am the most common tree of this collection, and the first level of the game.'};
-models['palm'] = {name: 'Palm', label: 'Palm', gltfModel: 'palm', id: 'treePalm', src: '/assets/gltf/palm.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'Palm trees are a botanical family of perennial lianas, shrubs, and trees. They are the only members of the family Arecaceae, which is the only family in the order Arecales. They grow in hot climates. '};
+models['palm'] = {name: 'Palm', label: 'Palm', gltfModel: 'palm', id: 'treePalm', src: '/assets/gltf/palm.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'Palm trees are a botanical family of perennial lianas, shrubs, and trees. They are the only members of the family Arecaceae, which is the only family in the order Arecales. They grow in hot climates.'};
+models['sycamore'] = {name: 'Sycamore', label: 'Sycamore', gltfModel: 'sycamore', id: 'treeSycamore', src: '/assets/gltf/sycamore.glb', posY: 0.310, minY: 0, scaleX: 1, scaleY: 1, scaleZ: -1, info: 'Acer pseudoplatanus, known as the sycamore in the United Kingdom and the sycamore maple in the United States,[2] is a flowering plant species in the soapberry and lychee family Sapindaceae.'};
+
 
 models['forest'] = {name: 'Forest', label: 'Forest', gltfModel: 'forest', id: 'treeForest', src: '/assets/gltf/forest.glb', posY: 0.310, minY: 0, scaleX: 3.150, scaleY: 3.270, scaleZ: -2.660, info: 'I am the forest :)'};
 
@@ -53,6 +55,6 @@ export function getTreeModelByLevel(level: number) {
         level = levels.length-1;
     }
 
-    //level = 2;
+    //level = 3;
     return parseTreeModel(models[levels[level]]);
 }
